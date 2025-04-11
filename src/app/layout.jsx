97cf13e2a38a14d7015011ab447f22a97d4c4127
“joanomana” <joansebastianomasu@gmail.com>
2025-04-11 13:45:07 -0500

    feat: :tada: Create init files

---
+++ src/app/layout.jsx
@@ -0,0 +1,19 @@
+
+import "./globals.css";
+
+
+
+export const metadata = {
+  title: "Dungeons & Dragons",
+  description: "Create your own D&D character",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+}
